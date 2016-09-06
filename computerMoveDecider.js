8a70010db0fe46d52c0d3ea6269000715d165202
Mark Robertson <markrivasrobertson@yahoo.com>
2016-09-05 20:18:50 -0400

    remove more console logs

--- computerMoveDecider.js
+++ computerMoveDecider.js
@@ -15,8 +15,6 @@ class ComputerMoveDecider {
 		this.expectedCount += expectedHidden;
 		let outcome;
 		this.expectedCount >= instances ? outcome = true : outcome = false;
-		console.log('outcome ='+outcome);
-		console.log('this.expectedCount ='+this.expectedCount);
 		return outcome;
 	}
 	wagerFaceValueChoice(instances, faceValue) {
@@ -30,13 +28,11 @@ class ComputerMoveDecider {
 		} else {
 			outcome = faceValue+1;
 		}
-		console.log(`face ${outcome}`);
 		return outcome;
 	}
 	wagerInstanceChoice(instances, chosenFace, currentFace) {
 		let outcome = 0;
-		chosenFace>currentFace ? outcome = 1 : outcome = instances+=1
-		console.log(`instances ${outcome}`);
+		chosenFace>currentFace ? outcome = 1 : outcome = instances+=1;
 		return outcome;	
 	}
 }
